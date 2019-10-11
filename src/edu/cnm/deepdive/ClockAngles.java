5c7fcff35f66301ede181db81c9dbdea334fa81a
Khasiano <NewbieCoder10>
2019-10-11 11:27:03 -0500

    Initial commit of Clock Angles

--- src/edu/cnm/deepdive/ClockAngles.java
+++ src/edu/cnm/deepdive/ClockAngles.java
@@ -37,6 +37,51 @@ public class ClockAngles {
    * @return        angle (in degrees) to which minute hand is oriented.
    */
   public static double minuteHandDegrees(double minutes) {
+    class ClockAngle {
+
+      private static final double HOUR_ANGLE = 360.0 / 12;
+      private static final double MINUTE_ANGLE = 360.0 / 60;
+      private static final double HOUR_DELTA_ANGLE = HOUR_ANGLE / 60;
+      private static final double MINUTE_DELTA_ANGLE = MINUTE_ANGLE / 60;
+
+      private final int angle;
+      private final int hourAngle;
+      private final int minAngle;
+
+      public ClockAngle(final int hour, final int min) {
+        this(hour, min, 0);
+      }
+
+      public ClockAngle(final int hour, final int min, final int sec) {
+
+        if (hour < 0 || hour > 23 || min < 0 || min > 59 || sec < 0 || sec > 59) {
+          throw new IllegalArgumentException("Invalid hours, minutes or seconds value");
+        }
+
+        this.minAngle = (int) (min * MINUTE_ANGLE + MINUTE_DELTA_ANGLE * sec);
+        this.hourAngle = (int) (hour % 12 * HOUR_ANGLE + HOUR_DELTA_ANGLE * min);
+
+        int angle = hourAngle - minAngle;
+
+        if (angle < 0) {
+          angle = 360 + angle;
+        }
+
+        this.angle = angle;
+      }
+
+      public int getHourAngle() { return hourAngle; }
+      public int getMinAngle() { return minAngle; }
+
+      /** @return angle in degrees from the minute to the hour hand */
+      public int getMinHourAngle() { return this.angle; }
+
+      /** @return angle in degrees from the hour to the minute hand */
+      public int getHourMinAngle() { return 360 - this.angle; }
+
+      /** @return {@code true} if the minute hand is on top of the hour hand; otherwise {@code false} */
+      public boolean isMinOverHour() { return this.angle == 0; }
+    }
     return 0; // TODO Replace this line with implementation.
   }
 
---
+++ src/edu/cnm/deepdive/ClockAnglesTest.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClockAnglesTest {
+
+  @org.junit.jupiter.api.Test
+  void hourHandDegrees() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void minuteHandDegrees() {
+  }
+}
